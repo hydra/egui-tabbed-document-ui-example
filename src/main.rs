544e88ea70ba1ee4942bf7f6055623cd8dd03b10
Dominic Clifton <me@dominicclifton.name>
2025-02-06 15:16:15 +0100

    Set window title.

--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,7 @@ fn main() -> eframe::Result {
         ..Default::default()
     };
     eframe::run_native(
-        "eframe template",
+        "Tabbed document UI",
         native_options,
         Box::new(|cc| Ok(Box::new(tabbed_document_ui_example::TemplateApp::new(cc)))),
     )

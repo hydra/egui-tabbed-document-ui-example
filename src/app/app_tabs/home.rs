cea01c6d0619458e70be8c3b05f7111bd9a9e7f5
Dominic Clifton <me@dominicclifton.name>
2025-02-07 11:34:43 +0100

    Use egui_flex to beautify the home tab.

--- src/app/app_tabs/home.rs
+++ src/app/app_tabs/home.rs
@@ -1,5 +1,7 @@
+use eframe::Frame;
 use crate::app::tabs::{Tab, TabKey};
-use egui::{FontFamily, Label, RichText, Ui, Widget, WidgetText};
+use egui::{Direction, FontFamily, Label, RichText, Ui, Widget, WidgetText};
+use egui_flex::{Flex, FlexAlign, FlexAlignContent, FlexItem, FlexJustify};
 use egui_material_icons::icons::ICON_HOME;
 use serde::{Deserialize, Serialize};
 
@@ -12,15 +14,48 @@ impl Tab for HomeTab {
     }
 
     fn ui(&mut self, ui: &mut Ui, _tab_key: &mut TabKey) {
-        ui.horizontal(|ui| {
-            Label::new(
-                RichText::new(ICON_HOME)
-                    .size(16.0)
-                    .family(FontFamily::Proportional),
-            )
-                .ui(ui);
+        let frame = egui::frame::Frame::group(ui.style());
 
-            ui.label("Home");
+        Flex::new()
+            .justify(FlexJustify::Center)
+            .h_full()
+            .w_full()
+            .show(ui, |flex| {
+
+                flex.add_ui(
+                    FlexItem::new()
+                        // causes the box of the frame to shrink to the content
+                        .align_self(FlexAlign::Center)
+                        .frame(frame), |ui|
+                {
+                    Label::new(
+                        RichText::new(ICON_HOME)
+                            .size(48.0)
+                            .family(FontFamily::Proportional),
+                    )
+                        .ui(ui);
+
+                    ui.label(RichText::new("Home").size(48.0));
+                });
+            });
+
+        // Without flex, doesn't center the content.
+        /*
+        ui.vertical_centered(|ui|{
+            ui.horizontal(|ui| {
+                egui::frame::Frame::new()
+                    .show(ui, |ui| {
+                        Label::new(
+                            RichText::new(ICON_HOME)
+                                .size(48.0)
+                                .family(FontFamily::Proportional),
+                        )
+                            .ui(ui);
+
+                        ui.label(RichText::new("Home").size(48.0));
+                    });
+            });
         });
+         */
     }
 }

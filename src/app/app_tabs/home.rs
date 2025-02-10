6177232336f7353cb379dbdbd89b682f6688a0f9
Dominic Clifton <me@dominicclifton.name>
2025-02-10 19:39:00 +0100

    Use 'add_flex' instead of `show_in`. Use 'add' instead of 'add_ui'.
    * as per suggestions from @lucasmerlin in discord - https://discord.com/channels/900275882684477440/1337452132516892806/1338264676156571719

--- src/app/app_tabs/home.rs
+++ src/app/app_tabs/home.rs
@@ -1,6 +1,6 @@
 use crate::app::tabs::{Tab, TabKey};
-use egui::{FontFamily, Label, RichText, Ui, Widget, WidgetText};
-use egui_flex::{Flex, FlexAlign, FlexDirection, FlexItem, FlexJustify};
+use egui::{Checkbox, FontFamily, Frame, Label, RichText, Ui, Widget, WidgetText};
+use egui_flex::{item, Flex, FlexAlign, FlexDirection, FlexItem, FlexJustify};
 use egui_i18n::tr;
 use egui_material_icons::icons::ICON_HOME;
 use serde::{Deserialize, Serialize};
@@ -22,40 +22,30 @@ impl<'a> Tab<Context<'a>> for HomeTab {
         Flex::new()
             .justify(FlexJustify::Center)
             .direction(FlexDirection::Vertical)
+            .align_items(FlexAlign::Center)
             .h_full()
             .w_full()
             .show(ui, |outer_flex| {
-                Flex::new()
-                    .justify(FlexJustify::Center)
-                    .w_full()
-                    .show_in(outer_flex, FlexItem::new(), |flex| {
-                        flex.add_ui(
-                            FlexItem::new()
-                                .frame(frame),
-                            |ui| {
-                                Label::new(
-                                    RichText::new(ICON_HOME)
-                                        .size(48.0)
-                                        .family(FontFamily::Proportional),
-                                )
-                                    .ui(ui);
+                outer_flex.add_flex(item().frame(frame), Flex::horizontal(), |flex| {
+                    flex.add(
+                        item(),
+                        Label::new(
+                            RichText::new(ICON_HOME)
+                                .size(48.0)
+                                .family(FontFamily::Proportional),
+                        ),
+                    );
+                    flex.add(
+                        item(),
+                        Label::new(
+                            RichText::new(tr!("home-heading"))
+                                .size(48.0)
+                                .family(FontFamily::Proportional),
+                        ),
+                    );
+                });
 
-                                ui.label(RichText::new(tr!("home-heading")).size(48.0));
-                            },
-                        );
-                    });
-
-                Flex::new()
-                    .justify(FlexJustify::Center)
-                    .w_full()
-                    .show_in(outer_flex, FlexItem::new()
-                        .align_self(FlexAlign::Center), |flex| {
-                        flex.add_ui(FlexItem::new(), |ui|{
-                            ui.checkbox(&mut context.config.show_home_tab_on_startup, tr!("home-tab-show-on-startup"));
-                        });
-                    });
+                outer_flex.add(item(), Checkbox::new(&mut context.config.show_home_tab_on_startup, tr!("home-tab-show-on-startup")));
             });
-
-
     }
 }
